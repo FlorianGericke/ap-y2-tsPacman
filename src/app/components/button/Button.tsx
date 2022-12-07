d6eb851b68609256e44dc5a6c43bee11434e9c5d
fgericke <florian.gericke@endava.com>
2022-12-07 15:45:25 +0100

    Botton Component Refactored

--- src/app/components/button/Button.tsx
+++ src/app/components/button/Button.tsx
@@ -3,22 +3,20 @@ import './scss/Button.scss';
 
 interface Clickable {
 	className?: string;
-	onClick?: (event?: MouseEvent) => void;
+	onClick?: () => void;
 	children?: string;
 }
 
 export const Button: React.FC<Clickable> = (props) => {
-	const [mouseDown, setMouseDown] = useState(false);
+	const [mouseDown, setMouseDown] = useState(false); // OnKlick Style Effect
 
 	return (
-		<div
-			className={props.className}
-			onClick={() => props.onClick && props.onClick()}
-		>
+		<div className={props.className}>
 			<div
 				className={
 					mouseDown ? 'Div-buttonInner--mousedown' : 'Div-buttonInner'
 				}
+				onClick={props.onClick}
 				onMouseDown={() => setMouseDown(true)}
 				onMouseUp={() => setMouseDown(false)}
 			>
